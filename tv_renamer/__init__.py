0a121ff5e44505ed0abdb72468f674e4907b2686
Filip 'AkaBlur' Vukoja <5957216+AkaBlur@users.noreply.github.com>
2026-05-16 15:13:58 +0200

    up; app startup

--- tv_renamer/__init__.py
+++ tv_renamer/__init__.py
@@ -4,13 +4,14 @@ import sys
 from . import app
 
 
-def run() -> None:
+def run() -> None:  # pragma: no cover
     """Application startup"""
-    WorkPath: Path = None
+    # set working directory to supplied directory if correct
     if len(sys.argv) == 2 and Path(sys.argv[1]).exists() and Path(sys.argv[1]).is_dir():  # noqa : PLR2004
-        WorkPath = Path(sys.argv[1])
+        App = app.RenamerApp(Path(sys.argv[1]))
 
-    App = app.RenamerApp(Path(WorkPath)) if WorkPath is not None else app.RenamerApp()
+    else:
+        App = app.RenamerApp()
 
     App.load_settings()
     App.register_callbacks()
